630b6409b6fcc06ae6f51b11c0ba5b135128bcb2
Mark Farrell <m4farrel@uwaterloo.ca>
2020-01-05 06:43:10 +0000

    Sensor.writeEntry

--- src/Sensor.purs
+++ src/Sensor.purs
@@ -1,5 +1,8 @@
 module Sensor
   ( Entry (..)
+  , parseEntry
+  , writeEntry
+  , insert
   ) where
 
 import Prelude
@@ -9,12 +12,20 @@ import Control.Monad.Trans.Class (lift)
 import Effect (Effect)
 import Effect.Class (liftEffect)
 
+import Effect.Exception (Error)
+import Effect.Exception (error) as Exception
+
+import Data.Either (Either(..))
+
+import Data.Identity (Identity)
+import Data.Newtype (unwrap)
+
 import Data.Foldable (foldl)
 import Data.Traversable(foldMap)
 import Data.String.CodeUnits (singleton)
 import Data.List(many)
 
-import Text.Parsing.Parser (Parser)
+import Text.Parsing.Parser (Parser, runParser)
 import Text.Parsing.Parser.String (char, satisfy)
 
 import Date as Date
@@ -138,3 +149,39 @@ insert entry = \req -> do
   query <- lift $ liftEffect $ insertQuery entry req
   DB.insert filename query
   where filename = "logs.db"
+
+writeEntry'' :: Entry -> String
+writeEntry'' (Entry entry) = foldl (\x y -> x <> delimiter' <> y) entry.sIP $
+  [ entry.dIP
+  , entry.sPort
+  , entry.dPort
+  , entry.protocol
+  , entry.packets
+  , entry.bytes
+  , entry.flags
+  , entry.sTime
+  , entry.duration
+  , entry.eTime
+  , entry.sensor
+  ]
+  where delimiter' = singleton delimiter
+
+writeEntry' :: Entry -> Identity (Either Error String)
+writeEntry' entry = do
+  expect     <- pure $ writeEntry'' entry
+  result'    <- pure $ flip runParser parseEntry $ (writeEntry'' entry)
+  case result' of
+    (Left error) -> do
+      let result'' = { error : error, expect : expect, entry : entry }
+      pure $ Left (Exception.error $ show result'')
+    (Right entry')    -> do
+      check <- pure $ writeEntry'' entry'
+      let result'' = { check  : check, expect : expect, entry : entry }
+      case check == expect of
+        true -> do
+          pure $ Right expect
+        false -> do
+          pure $ Left (Exception.error $ show result'')
+
+writeEntry :: Entry -> Either Error String
+writeEntry entry = unwrap $ writeEntry' entry
