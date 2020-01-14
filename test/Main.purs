b088729f015ad0075e139a405a5f0cff3b805940
Mark Farrell <m4farrel@uwaterloo.ca>
2020-01-14 15:21:19 +0000

    Test Sensor.schema

--- test/Main.purs
+++ test/Main.purs
@@ -10,13 +10,16 @@ import Effect.Aff (Aff, launchAff)
 import Assert (assert)
 
 import DB as DB
+import SIEM.Logging.Sensor as Sensor
 
-testRequest :: forall a. DB.Request a -> Aff Unit
-testRequest request = do
+runRequest' :: forall a. DB.Request a -> Aff Unit
+runRequest' request = do
   result <- DB.runRequest request
   assert true $ isRight result
 
 main :: Effect Unit
 main = void $ launchAff $ do
-  _ <- testRequest $ DB.touch ":memory:"
+  _ <- runRequest' $ DB.touch ":memory:"
+  _ <- runRequest' $ DB.touch "test.db"
+  _ <- runRequest' $ Sensor.schema "test.db"
   pure unit
