9d66b7b972898dd3549a7b0770e93d83379bede1
Mark Farrell <markfarr2011@gmail.com>
2020-03-03 10:23:49 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ build_directory='bin'
 build_name="server"
 echo "Building project to '$build_directory' ..."
 attempt "rm -rf $build_directory && mkdir $build_directory" "Initialize '$build_directory' directory"
-attempt "pulp build -m SIEM.Logging.Server --to bin/$build_name.js" "Compile $build_name" 
+attempt "pulp build -m Server --to bin/$build_name.js" "Compile $build_name" 
 cd $build_directory
-attempt "pkg $build_name.js -t node10-linux-x64,node10-macos-x64,node10-windows-x64" "Package binary executable(s) for $build_name"
+attempt "pkg $build_name.js -t linux-x64,macos-x64,windows-x64" "Package binary executable(s) for $build_name"
 attempt "rm -f *.js" "Clean up '$build_directory'"
 cd ..
 echo "Done."
