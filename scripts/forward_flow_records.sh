626020cb01cacef53d3463d7dd8ea9924ac6fd31
Mark Farrell <markfarr2011@gmail.com>
2020-03-04 21:23:09 +0000

    Merge scripts/forward_flow_records.sh, scripts/sample_flow_records.sh

--- scripts/forward_flow_records.sh
+++ scripts/forward_flow_records.sh
@@ -1,4 +1,43 @@
 #!/bin/sh
+sample_octet() {
+	echo "$(shuf -i 0-255 -n 1)"
+}
+sample_ipv4() {
+	A=$(sample_octet)
+	B=$(sample_octet)
+	C=$(sample_octet)
+	D=$(sample_octet)
+	echo "$A.$B.$C.$D"
+}
+sample_port() {
+	echo "$(shuf -i 0-65535 -n 1)"
+}
+sample_protocol() {
+	echo "$(sample_octet)"
+}
+sample_flow_record() {
+	SIP=$(sample_ipv4)
+	DIP=$(sample_ipv4)
+	SPORT=$(sample_port)
+	DPORT=$(sample_port)
+	PROTOCOL=$(sample_protocol)
+	PACKETS="0"
+	BYTES="0"
+	FLAGS=""
+	STIME="1970/01/01T00:00:00.000"
+	DURATION="0.000"
+	ETIME="1970/01/01T00:00:00.000"
+	SENSOR=""
+	echo "$SIP,$DIP,$SPORT,$DPORT,$PROTOCOL,$PACKETS,$BYTES,$FLAGS,$STIME,$DURATION,$ETIME,$SENSOR"
+}
 HOST=$1
-FLOW_RECORD=$2
-curl -G -v "http://$HOST/forward/flow" --data-urlencode "q=$FLOW_RECORD"
+N=$2
+for i in $(seq $N); do
+	FLOW_RECORD=$(sample_flow_record)
+	START_TIME=$(date +%s%N | cut -b1-13)
+	curl --fail -G -s "http://$HOST/forward/flow" --data-urlencode "q=$FLOW_RECORD"
+	RESULT=$?
+	END_TIME=$(date +%s%N | cut -b1-13)
+	DURATION=$(($END_TIME - $START_TIME))
+	echo "$RESULT,$DURATION,$FLOW_RECORD"
+done
