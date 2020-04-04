e07a68fbc314661fb0c3466f2903461085601771
Mark Farrell <markfarr2011@gmail.com>
2020-04-04 20:46:21 +0000

    FFI.JSON.parse

--- src/FFI/JSON.js
+++ src/FFI/JSON.js
@@ -1,3 +1,16 @@
 "use strict";
 
 exports.stringify = JSON.stringify;
+
+exports.parseImpl = function(left) {
+  return function(right) {
+    return function(input) {
+      try {
+        var obj = JSON.parse(input);
+        return right(obj);
+      } catch (err) {
+        return left(err);
+      }
+    };
+  };
+};
--- src/FFI/JSON.purs
+++ src/FFI/JSON.purs
@@ -1,7 +1,17 @@
 module FFI.JSON
   ( stringify
+  , parse
   ) where
+
+import Data.Either (Either(..))
 
 import Foreign (Foreign)
 
+import Effect.Exception(Error)
+
 foreign import stringify :: Foreign -> String
+
+foreign import parseImpl :: (Error -> Either Error Foreign) -> (Foreign -> Either Error Foreign) -> String -> Either Error Foreign
+
+parse :: String -> Either Error Foreign
+parse = parseImpl Left Right
