18a89623b6e30478997d6ca46c32e20b569530b6
Mark Farrell <markfarr2011@gmail.com>
2020-05-31 00:53:14 +0000

    Fix Text.Parsing.Repeat

--- src/Text/Parsing/Repeat.purs
+++ src/Text/Parsing/Repeat.purs
@@ -1,8 +1,6 @@
 module Text.Parsing.Repeat
   ( until
-  , maximum
   , exact
-  , minimum
   ) where
 
 import Prelude
@@ -24,45 +22,15 @@ until = until' []
         true  -> pure acc
         false -> until' (acc <> [x]) u v
 
-{-- | Repeats a parser *u* at most *n* times. --}
-maximum :: forall a b. Int -> Parser a b -> Parser a (Array b)
-maximum = \n u -> case n > 0 of
-  false  -> fail "Maximum repeat limit must be positive."
-  true   -> maximum' [] n u
-  where
-    maximum' acc n u = case n > 0 of
-      false -> pure acc
-      true -> do
-        x <- u 
-        y <- maximum' (acc <> [x]) (n - 1) u
-        pure y
-
 {-- | Repeats a parser *u* exactly *n* times. --}
 exact :: forall a b. Int -> Parser a b -> Parser a (Array b)
 exact n = \u -> case n > 0 of
   false  -> fail "Exact repeat limit must be positive."
-  true   -> exact' [] n u
+  true   -> exact' [] 0 u
   where
-    exact' acc m u = case m > 0 of
-      false -> case (n - m) == n of
-        false -> fail $ "Invalid exact # of occurrences (" <> show m <> "," <> show n <> ")."
-        true  -> pure acc
-      true -> do
+    exact' acc m u = case m == n of
+      true  -> pure acc
+      false -> do
         x <- u 
-        y <- exact' (acc <> [x]) (m - 1) u
-        pure y
-
-{-- | Repeats a parser *u* at least *n* times. --}
-minimum :: forall a b. Int -> Parser a b -> Parser a (Array b)
-minimum n = \u -> case n > 0 of
-  false  -> fail "Minimum repeat limit must be positive."
-  true   -> minimum' [] n u
-  where
-    minimum' acc m u = case m > 0 of
-      false -> case (n - m) <= n of
-        false -> fail $ "Invalid least # of occurrences (" <> show m <> "," <> show n <> ")."
-        true  -> pure acc
-      true -> do
-        x <- u 
-        y <- minimum' (acc <> [x]) (m - 1) u
+        y <- exact' (acc <> [x]) (m + 1) u
         pure y
